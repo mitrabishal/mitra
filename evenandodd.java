49bb6ee8b78e1fc3650735e4e0795496f4ee3d1b
mitrabishal <bishal.mitra.683@gmail.com>
2017-07-09 15:16:21 +0530

    Add files via upload

---
+++ evenandodd.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+	int x;
+	x=s.nextInt();
+	if(x%2==0)
+	{
+		System.out.println("The Number is Even");
+	}
+	else
+	{
+		System.out.println("The Number is Odd");
+	}
+	}
+}
